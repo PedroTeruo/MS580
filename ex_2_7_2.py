5fe8cb6498bd490bcd8dda67d1de72abeebfcb24
CHARLIE ALVES CRUZ BRAMBILHA (RA 198174) <ra198174@IME.UNICAMP.BR>
2026-09-10 11:10:57 -0300

    sla

--- ex_2_7_2.py
+++ ex_2_7_2.py
@@ -1,87 +1,94 @@
-import pandas as pd
-import skfuzzy as fuzzy
-import numpy as np
-from skfuzzy import control as ctrl
-import matplotlib.pyplot as plt
+import pandas as pd;
+import skfuzzy as fuzzy;
+import numpy as np;
+from skfuzzy import control as ctrl;
+import matplotlib.pyplot as plt;
 
 #EXERCÍCIO 2.7.2
 
 #---------------------------------------------------------
 # Definindo os domínios
-massa = ctrl.Antecedent(np.arange(47, 81.1, 0.1), 'Massa')
-altura = ctrl.Antecedent(np.arange(157, 184, 1), 'Altura')
-grau_risco = ctrl.Consequent(np.arange(18, 36, 1), 'Grau de Risco')
+
+massa = ctrl.Antecedent(np.arange(47, 81.1, 0.1), 'Massa');
+altura = ctrl.Antecedent(np.arange(157, 184, 1), 'Altura');
+grau_risco = ctrl.Consequent(np.arange(18, 36, 1), 'Grau de Risco');
 
 #---------------------------------------------------------
 # Definindo as faixas de pertinência
-massa['Baixa'] = fuzzy.trapmf(massa.universe, [47, 47, 55, 64])
-massa['Média Baixa'] = fuzzy.trimf(massa.universe, [50, 59, 68])
-massa['Média'] = fuzzy.trimf(massa.universe, [53, 62.5, 72])
-massa['Média Alta'] = fuzzy.trimf(massa.universe, [56, 66.5, 77])
-massa['Alta'] = fuzzy.trapmf(massa.universe, [59, 70, 81, 81])
 
-altura['Baixa'] = fuzzy.trapmf(altura.universe, [157, 157, 160, 163])
-altura['Média Baixa'] = fuzzy.trimf(altura.universe, [162, 165, 168])
-altura['Média'] = fuzzy.trimf(altura.universe, [167, 170, 173])
-altura['Média Alta'] = fuzzy.trimf(altura.universe, [172, 175, 178])
-altura['Alta'] = fuzzy.trapmf(altura.universe, [177, 180, 183, 183])
+massa['Baixa'] = fuzzy.trapmf(massa.universe, [47, 47, 55, 64]);
+massa['Média Baixa'] = fuzzy.trimf(massa.universe, [50, 59, 68]);
+massa['Média'] = fuzzy.trimf(massa.universe, [53, 62.5, 72]);
+massa['Média Alta'] = fuzzy.trimf(massa.universe, [56, 66.5, 77]);
+massa['Alta'] = fuzzy.trapmf(massa.universe, [59, 70, 81, 81]);
 
-grau_risco['Saudável'] = fuzzy.trapmf(grau_risco.universe, [18, 18, 23, 25])
-grau_risco['Moderado'] = fuzzy.trapmf(grau_risco.universe, [24, 25, 27, 30])
-grau_risco['Alto'] = fuzzy.trapmf(grau_risco.universe, [29, 30, 35, 35])
+altura['Baixa'] = fuzzy.trapmf(altura.universe, [157, 157, 160, 163]);
+altura['Média Baixa'] = fuzzy.trimf(altura.universe, [162, 165, 168]);
+altura['Média'] = fuzzy.trimf(altura.universe, [167, 170, 173]);
+altura['Média Alta'] = fuzzy.trimf(altura.universe, [172, 175, 178]);
+altura['Alta'] = fuzzy.trapmf(altura.universe, [177, 180, 183, 183]);
+
+grau_risco['Saudável'] = fuzzy.trapmf(grau_risco.universe, [18, 18, 23, 25]);
+grau_risco['Moderado'] = fuzzy.trapmf(grau_risco.universe, [24, 25, 27, 30]);
+grau_risco['Alto'] = fuzzy.trapmf(grau_risco.universe, [29, 30, 35, 35]);
 
 #---------------------------------------------------------
 # Salvando os gráficos em png
 '''
-massa.view()
-altura.view()
-grau_risco.view()
+massa.view();
+altura.view();
+grau_risco.view();
 '''
 #---------------------------------------------------------
 # Definindo as regras
-r1 = ctrl.Rule(massa['Baixa'] & altura['Média'], grau_risco['Saudável'])
-r2 = ctrl.Rule(massa['Baixa'] & altura['Média Baixa'], grau_risco['Saudável'])
-r3 = ctrl.Rule(massa['Baixa'] & altura['Média'], grau_risco['Saudável'])
-r4 = ctrl.Rule(massa['Baixa'] & altura['Média Alta'], grau_risco['Saudável'])
-r5 = ctrl.Rule(massa['Baixa'] & altura['Alta'], grau_risco['Saudável'])
 
-r6 = ctrl.Rule(massa['Média Baixa'] & altura['Média'], grau_risco['Moderado'])
-r7 = ctrl.Rule(massa['Média Baixa'] & altura['Média Baixa'], grau_risco['Saudável'])
-r8 = ctrl.Rule(massa['Média Baixa'] & altura['Média'], grau_risco['Saudável'])
-r9 = ctrl.Rule(massa['Média Baixa'] & altura['Média Alta'], grau_risco['Saudável'])
-r10 = ctrl.Rule(massa['Média Baixa'] & altura['Alta'], grau_risco['Saudável'])
+conjunto_regras = [];
 
-r11 = ctrl.Rule(massa['Média'] & altura['Média'], grau_risco['Moderado'])
-r12 = ctrl.Rule(massa['Média'] & altura['Média Baixa'], grau_risco['Moderado'])
-r13 = ctrl.Rule(massa['Média'] & altura['Média'], grau_risco['Saudável'])
-r14 = ctrl.Rule(massa['Média'] & altura['Média Alta'], grau_risco['Saudável'])
-r15 = ctrl.Rule(massa['Média'] & altura['Alta'], grau_risco['Saudável'])
+conjunto_regras.append(ctrl.Rule(massa['Baixa'] & altura['Média'], grau_risco['Saudável']));
+conjunto_regras.append(ctrl.Rule(massa['Baixa'] & altura['Média Baixa'], grau_risco['Saudável']));
+conjunto_regras.append(ctrl.Rule(massa['Baixa'] & altura['Média'], grau_risco['Saudável']));
+conjunto_regras.append(ctrl.Rule(massa['Baixa'] & altura['Média Alta'], grau_risco['Saudável']));
+conjunto_regras.append(ctrl.Rule(massa['Baixa'] & altura['Alta'], grau_risco['Saudável']));
 
-r16 = ctrl.Rule(massa['Média Alta'] & altura['Média'], grau_risco['Moderado'])
-r17 = ctrl.Rule(massa['Média Alta'] & altura['Média Baixa'], grau_risco['Moderado'])
-r18 = ctrl.Rule(massa['Média Alta'] & altura['Média'], grau_risco['Moderado'])
-r19 = ctrl.Rule(massa['Média Alta'] & altura['Média Alta'], grau_risco['Saudável'])
-r20 = ctrl.Rule(massa['Média Alta'] & altura['Alta'], grau_risco['Saudável'])
+conjunto_regras.append(ctrl.Rule(massa['Média Baixa'] & altura['Média'], grau_risco['Moderado']));
+conjunto_regras.append(ctrl.Rule(massa['Média Baixa'] & altura['Média Baixa'], grau_risco['Saudável']));
+conjunto_regras.append(ctrl.Rule(massa['Média Baixa'] & altura['Média'], grau_risco['Saudável']));
+conjunto_regras.append(ctrl.Rule(massa['Média Baixa'] & altura['Média Alta'], grau_risco['Saudável']));
+conjunto_regras.append(ctrl.Rule(massa['Média Baixa'] & altura['Alta'], grau_risco['Saudável']));
 
-r21 = ctrl.Rule(massa['Alta'] & altura['Média'], grau_risco['Alto'])
-r22 = ctrl.Rule(massa['Alta'] & altura['Média Baixa'], grau_risco['Moderado'])
-r23 = ctrl.Rule(massa['Alta'] & altura['Média'], grau_risco['Moderado'])
-r24 = ctrl.Rule(massa['Alta'] & altura['Média Alta'], grau_risco['Moderado'])
-r25 = ctrl.Rule(massa['Alta'] & altura['Alta'], grau_risco['Saudável'])
+conjunto_regras.append(ctrl.Rule(massa['Média'] & altura['Média'], grau_risco['Moderado']));
+conjunto_regras.append(ctrl.Rule(massa['Média'] & altura['Média Baixa'], grau_risco['Moderado']));
+conjunto_regras.append(ctrl.Rule(massa['Média'] & altura['Média'], grau_risco['Saudável']));
+conjunto_regras.append(ctrl.Rule(massa['Média'] & altura['Média Alta'], grau_risco['Saudável']));
+conjunto_regras.append(ctrl.Rule(massa['Média'] & altura['Alta'], grau_risco['Saudável']));
 
-conjunto_regras = [r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25]
+conjunto_regras.append(ctrl.Rule(massa['Média Alta'] & altura['Média'], grau_risco['Moderado']));
+conjunto_regras.append(ctrl.Rule(massa['Média Alta'] & altura['Média Baixa'], grau_risco['Moderado']));
+conjunto_regras.append( ctrl.Rule(massa['Média Alta'] & altura['Média'], grau_risco['Moderado']));
+conjunto_regras.append(ctrl.Rule(massa['Média Alta'] & altura['Média Alta'], grau_risco['Saudável']));
+conjunto_regras.append(ctrl.Rule(massa['Média Alta'] & altura['Alta'], grau_risco['Saudável']));
+
+conjunto_regras.append(ctrl.Rule(massa['Alta'] & altura['Média'], grau_risco['Alto']));
+conjunto_regras.append(ctrl.Rule(massa['Alta'] & altura['Média Baixa'], grau_risco['Moderado']));
+conjunto_regras.append(ctrl.Rule(massa['Alta'] & altura['Média'], grau_risco['Moderado']));
+conjunto_regras.append(ctrl.Rule(massa['Alta'] & altura['Média Alta'], grau_risco['Moderado']));
+conjunto_regras.append(ctrl.Rule(massa['Alta'] & altura['Alta'], grau_risco['Saudável']));
+
 #---------------------------------------------------------
 # Criando o sistema de regras do controle
-grau_risco_ctrl = ctrl.ControlSystem(conjunto_regras)
-grau_risco_simulacao = ctrl.ControlSystemSimulation(grau_risco_ctrl)
+
+grau_risco_ctrl = ctrl.ControlSystem(conjunto_regras);
+grau_risco_simulacao = ctrl.ControlSystemSimulation(grau_risco_ctrl);
 
 #---------------------------------------------------------
 # Inputs
-grau_risco_simulacao.input['Massa'] = 81.0  # (float(input()))
-grau_risco_simulacao.input['Altura'] = 180 # (int(input()))
+
+grau_risco_simulacao.input['Massa'] = 59.0;  # (float(input()))
+grau_risco_simulacao.input['Altura'] = 164; # (int(input()))
 
 #---------------------------------------------------------
 # Saída
-grau_risco_simulacao.compute()
-print(grau_risco_simulacao.output['Grau de Risco'])
-# ERRO de ~0.3
+
+grau_risco_simulacao.compute();
+print(grau_risco_simulacao.output['Grau de Risco']);
+# ERRO de ~0.5
--- ex_2_7_3.py
+++ ex_2_7_3.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt;
 
 #---------------------------------------------------------
 # Definindo os domínios
+
 cor_aparente = ctrl.Antecedent(np.arange(0, 31, 1), 'Cor Aparente');
 ph = ctrl.Antecedent(np.arange(0, 14.1, 0.1), 'pH');
 turbidez = ctrl.Antecedent(np.arange(0, 10.1, 0.1), 'Turbidez');
@@ -15,19 +16,20 @@ qualidade_agua = ctrl.Consequent(np.arange(0, 1.1, 0.1), 'Qualidade da Água');
 
 #---------------------------------------------------------
 # Definindo as faixas de pertinência
+
 cor_aparente['Boa'] = fuzzy.trapmf(cor_aparente.universe, [0, 0, 4, 6]);
 cor_aparente['Adequada'] = fuzzy.trapmf(cor_aparente.universe, [4, 6, 14, 16]);
 cor_aparente['Inadequada'] = fuzzy.trapmf(cor_aparente.universe, [14, 16, 30, 30]);
 
 ph['Inadequado Baixo'] = fuzzy.trapmf(ph.universe, [0, 0, 5.3,6 ]);
-ph['Adequado Baixo'] = fuzzy.trapmf(cor_aparente.universe, [5.6, 6, 6.3, 6.5);
-ph['Bom'] = fuzzy.trapmf(cor_aparente.universe, [6.3, 6.7, 8.5, 9]);
-ph['Adequado Alto'] = fuzzy.trapmf(cor_aparente.universe, [8.5, 8.9, 9.6, 10]);
+ph['Adequado Baixo'] = fuzzy.trapmf(ph.universe, [5.6, 6, 6.3, 6.5]);
+ph['Bom'] = fuzzy.trapmf(ph.universe, [6.3, 6.7, 8.5, 9]);
+ph['Adequado Alto'] = fuzzy.trapmf(ph.universe, [8.5, 8.9, 9.6, 10]);
 ph['Inadequado Alto'] = fuzzy.trapmf(ph.universe, [9.6, 10 ,14 ,14 ]);
 
-turbidez['Boa'] = fuzzy.trapmf(ph.universe, [0, 0, 0.5, 1.5]);
-turbidez['Adequada'] = fuzzy.trapmf(ph.universe, [0.5, 1.5, 4.5, 5.5]);
-turbidez['Inadequada'] = fuzzy.trapmf(ph.universe, [4.5, 5.5, 10, 10]);
+turbidez['Boa'] = fuzzy.trapmf(turbidez.universe, [0, 0, 0.5, 1.5]);
+turbidez['Adequada'] = fuzzy.trapmf(turbidez.universe, [0.5, 1.5, 4.5, 5.5]);
+turbidez['Inadequada'] = fuzzy.trapmf(turbidez.universe, [4.5, 5.5, 10, 10]);
 
 qualidade_agua['Boa'] = fuzzy.trapmf(qualidade_agua.universe, [0, 0, 0.4, 0.5]);
 qualidade_agua['Adequada'] = fuzzy.trapmf(qualidade_agua.universe, [0.4, 0.5, 0.7, 0.8]);
@@ -43,67 +45,73 @@ qualidade_agua.view();
 '''
 #---------------------------------------------------------
 # Definindo as regras
-r1 = ctrl.Rule(cor_aparente['Boa'] & ph['Inadequado Baixo'] & turbidez['Boa'], qualidade_agua['Inadequada']);
-r2 = ctrl.Rule(cor_aparente['Boa'] & ph['Inadequado Baixo'] & turbidez['Adequada'], qualidade_agua['Inadequada']);
-r3 = ctrl.Rule(cor_aparente['Boa'] & ph['Inadequado Baixo'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
-r4 = ctrl.Rule(cor_aparente['Boa'] & ph['Adequado Baixo'] & turbidez['Boa'], qualidade_agua['Adequada']);
-r5 = ctrl.Rule(cor_aparente['Boa'] & ph['Adequado Baixo'] & turbidez['Adequada'], qualidade_agua['Adequada']);
-r6 = ctrl.Rule(cor_aparente['Boa'] & ph['Adequado Baixo'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
-r7 = ctrl.Rule(cor_aparente['Boa'] & ph['Bom'] & turbidez['Boa'], qualidade_agua['Boa']);
-r8 = ctrl.Rule(cor_aparente['Boa'] & ph['Bom'] & turbidez['Adequada'], qualidade_agua['Boa']);
-r9 = ctrl.Rule(cor_aparente['Boa'] & ph['Bom'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
-r10 = ctrl.Rule(cor_aparente['Boa'] & ph['Adequado Alto'] & turbidez['Boa'], qualidade_agua['Adequada']);
-r11 = ctrl.Rule(cor_aparente['Boa'] & ph['Adequado Alto'] & turbidez['Adequada'], qualidade_agua['Adequada']);
-r12 = ctrl.Rule(cor_aparente['Boa'] & ph['Adequado Alto'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
-r13 = ctrl.Rule(cor_aparente['Boa'] & ph['Inadequado Alto'] & turbidez['Boa'], qualidade_agua['Inadequada']);
-r14 = ctrl.Rule(cor_aparente['Boa'] & ph['Inadequado Alto'] & turbidez['Adequada'], qualidade_agua['Inadequada']);
-r15 = ctrl.Rule(cor_aparente['Boa'] & ph['Inadequado Alto'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
 
-r16 = ctrl.Rule(cor_aparente['Adequada'] & ph['Inadequado Baixo'] & turbidez['Boa'], qualidade_agua['Inadequada']);
-r17 = ctrl.Rule(cor_aparente['Adequada'] & ph['Inadequado Baixo'] & turbidez['Adequada'], qualidade_agua['Inadequada']);
-r18 = ctrl.Rule(cor_aparente['Adequada'] & ph['Inadequado Baixo'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
-r19 = ctrl.Rule(cor_aparente['Adequada'] & ph['Adequado Baixo'] & turbidez['Boa'], qualidade_agua['Adequada']);
-r20 = ctrl.Rule(cor_aparente['Adequada'] & ph['Adequado Baixo'] & turbidez['Adequada'], qualidade_agua['Adequada']);
-r21 = ctrl.Rule(cor_aparente['Adequada'] & ph['Adequado Baixo'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
-r22 = ctrl.Rule(cor_aparente['Adequada'] & ph['Bom'] & turbidez['Boa'], qualidade_agua['Boa']);
-r23 = ctrl.Rule(cor_aparente['Adequada'] & ph['Bom'] & turbidez['Adequada'], qualidade_agua['Adequada']);
-r24 = ctrl.Rule(cor_aparente['Adequada'] & ph['Bom'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
-r25 = ctrl.Rule(cor_aparente['Adequada'] & ph['Adequado Alto'] & turbidez['Boa'], qualidade_agua['Adequada']);
-r26 = ctrl.Rule(cor_aparente['Adequada'] & ph['Adequado Alto'] & turbidez['Adequada'], qualidade_agua['Adequada']);
-r27 = ctrl.Rule(cor_aparente['Adequada'] & ph['Adequado Alto'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
-r28 = ctrl.Rule(cor_aparente['Adequada'] & ph['Inadequado Alto'] & turbidez['Boa'], qualidade_agua['Inadequada']);
-r29 = ctrl.Rule(cor_aparente['Adequada'] & ph['Inadequado Alto'] & turbidez['Adequada'], qualidade_agua['Inadequada']);
-r30 = ctrl.Rule(cor_aparente['Adequada'] & ph['Inadequado Alto'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
+conjunto_regras = [];
 
-r31 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Inadequado Baixo'] & turbidez['Boa'], qualidade_agua['Inadequada']);
-r32 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Inadequado Baixo'] & turbidez['Adequada'], qualidade_agua['Inadequada']);
-r33 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Inadequado Baixo'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
-r34 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Adequado Baixo'] & turbidez['Boa'], qualidade_agua['Inadequada']);
-r35 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Adequado Baixo'] & turbidez['Adequada'], qualidade_agua['Inadequada']);
-r36 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Adequado Baixo'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
-r37 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Bom'] & turbidez['Boa'], qualidade_agua['Adequada']);
-r38 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Bom'] & turbidez['Adequada'], qualidade_agua['Adequada']);
-r39 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Bom'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
-r40 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Adequado Alto'] & turbidez['Boa'], qualidade_agua['Inadequada']);
-r41 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Adequado Alto'] & turbidez['Adequada'], qualidade_agua['Inadequada']);
-r42 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Adequado Alto'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
-r43 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Inadequado Alto'] & turbidez['Boa'], qualidade_agua['Inadequada']);
-r44 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Inadequado Alto'] & turbidez['Adequada'], qualidade_agua['Inadequada']);
-r45 = ctrl.Rule(cor_aparente['Inadequada'] & ph['Inadequado Alto'] & turbidez['Inadequada'], qualidade_agua['Inadequada']);
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Inadequado Baixo'] & turbidez['Boa'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Inadequado Baixo'] & turbidez['Adequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Inadequado Baixo'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Adequado Baixo'] & turbidez['Boa'], qualidade_agua['Adequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Adequado Baixo'] & turbidez['Adequada'], qualidade_agua['Adequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Adequado Baixo'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Bom'] & turbidez['Boa'], qualidade_agua['Boa']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Bom'] & turbidez['Adequada'], qualidade_agua['Boa']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Bom'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Adequado Alto'] & turbidez['Boa'], qualidade_agua['Adequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Adequado Alto'] & turbidez['Adequada'], qualidade_agua['Adequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Adequado Alto'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Inadequado Alto'] & turbidez['Boa'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Inadequado Alto'] & turbidez['Adequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Boa'] & ph['Inadequado Alto'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
 
-conjunto_regras = [r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12];
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Inadequado Baixo'] & turbidez['Boa'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Inadequado Baixo'] & turbidez['Adequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Inadequado Baixo'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Adequado Baixo'] & turbidez['Boa'], qualidade_agua['Adequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Adequado Baixo'] & turbidez['Adequada'], qualidade_agua['Adequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Adequado Baixo'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Bom'] & turbidez['Boa'], qualidade_agua['Boa']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Bom'] & turbidez['Adequada'], qualidade_agua['Adequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Bom'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Adequado Alto'] & turbidez['Boa'], qualidade_agua['Adequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Adequado Alto'] & turbidez['Adequada'], qualidade_agua['Adequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Adequado Alto'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Inadequado Alto'] & turbidez['Boa'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Inadequado Alto'] & turbidez['Adequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Adequada'] & ph['Inadequado Alto'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Inadequado Baixo'] & turbidez['Boa'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Inadequado Baixo'] & turbidez['Adequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Inadequado Baixo'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Adequado Baixo'] & turbidez['Boa'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Adequado Baixo'] & turbidez['Adequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Adequado Baixo'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Bom'] & turbidez['Boa'], qualidade_agua['Adequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Bom'] & turbidez['Adequada'], qualidade_agua['Adequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Bom'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Adequado Alto'] & turbidez['Boa'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Adequado Alto'] & turbidez['Adequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Adequado Alto'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Inadequado Alto'] & turbidez['Boa'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Inadequado Alto'] & turbidez['Adequada'], qualidade_agua['Inadequada']));
+conjunto_regras.append(ctrl.Rule(cor_aparente['Inadequada'] & ph['Inadequado Alto'] & turbidez['Inadequada'], qualidade_agua['Inadequada']));
+
 #---------------------------------------------------------
 # Criando o sistema de regras do controle
+
 qualidade_agua_ctrl = ctrl.ControlSystem(conjunto_regras);
 qualidade_agua_simulacao = ctrl.ControlSystemSimulation(qualidade_agua_ctrl);
 
 #---------------------------------------------------------
 # Inputs
+
 qualidade_agua_simulacao.input['Cor Aparente'] = 15;  # (float(input()))
-qualidade_agua_simulacao.input['pH'] = 7;
-qualidade_agua_simulacao.input['Turbidez'] = 0; # (int(input()))
+qualidade_agua_simulacao.input['pH'] = 7;             # (float(input()))
+qualidade_agua_simulacao.input['Turbidez'] = 0;       # (float(input()))
 
 #---------------------------------------------------------
 # Saída
+
 qualidade_agua_simulacao.compute();
 print(qualidade_agua_simulacao.output['Qualidade da Água']);
+# ERRO de ~0.12
